e85a68fa5c682f536c9c6f0dca2325e0d7ea5d2c
Name <email@example.ru>
2024-02-27 12:42:18 +0300

    разработан метод ShowResult() и исправлены недочеты

--- Program.cs
+++ Program.cs
@@ -1,57 +1,57 @@
 ﻿static (string name, string surname, int age, int quantityOfPets, string[] nicknames, int quantityOfFavoriteColors, string[] favoriteColors) Survey()
 {
     (string name, string surname, int age, int quantityOfPets, string[] nicknames, int quantityOfFavoriteColors, string[] favoriteColors) survey;
-
     Console.Write("Введите имя: ");
     survey.name = Console.ReadLine();
 
     Console.Write("Введите фамилию: ");
     survey.surname = Console.ReadLine();
 
-    string age;
+    string inputAge;
     int intAge;
     do
     {
         Console.Write("Введите возраст: ");
-        age = Console.ReadLine();
+        inputAge = Console.ReadLine();
     }
-    while (CheckingValueCorrectEntered(age, out intAge));
+    while (CheckingValueCorrectEntered(inputAge, out intAge));
 
     survey.age = intAge;
-
 
     Console.Write("У вас есть домашний питомец? ");
     var HaveAPet = Console.ReadLine();
 
     if (HaveAPet == "да" || HaveAPet == "Да")
     {
-        string quantityOfPets;
+        string inputQuantityOfPets;
         int intQuantityOfPets;
         do
         {
             Console.Write("Сколько у вас питомцев? ");
-            quantityOfPets = Console.ReadLine();
+            inputQuantityOfPets = Console.ReadLine();
         }
-        while (CheckingValueCorrectEntered(quantityOfPets, out intQuantityOfPets));
+        while (CheckingValueCorrectEntered(inputQuantityOfPets, out intQuantityOfPets));
 
         survey.quantityOfPets = intQuantityOfPets;
 
+        Console.WriteLine("Как их зовут? ");
+
         survey.nicknames = NicknamesOfPets(survey.quantityOfPets);
     }
-
     else
     {
         survey.quantityOfPets = 0;
+        survey.nicknames = NicknamesOfPets(survey.quantityOfPets);
     }
 
-    string quantityOfFavoriteColors;
+    string inputQuantityOfFavoriteColors;
     int intQuantityOfFavoriteColors;
     do
     {
         Console.Write("Сколько у вас любимых цветов? ");
-        quantityOfFavoriteColors = Console.ReadLine();
+        inputQuantityOfFavoriteColors = Console.ReadLine();
     }
-    while (CheckingValueCorrectEntered(quantityOfFavoriteColors, out intQuantityOfFavoriteColors));
+    while (CheckingValueCorrectEntered(inputQuantityOfFavoriteColors, out intQuantityOfFavoriteColors));
 
     survey.quantityOfFavoriteColors = intQuantityOfFavoriteColors;
 
@@ -60,46 +60,67 @@
     return survey;
 }
 
-
 static string[] NicknamesOfPets(int quantityOfPets)
 {
     var nicknames = new string[quantityOfPets];
 
-    Console.Write("Как их зовут? ");
     for (int i = 0; i < nicknames.Length; i++)
         nicknames[i] = Console.ReadLine();
 
     return nicknames;
+
 }
 
 static string[] FavoriteColors(int quantityOfFavoriteColors)
 {
     string[] favoriteColors = new string[quantityOfFavoriteColors];
 
-    Console.Write("Какие? ");
+    Console.WriteLine("Какие? ");
     for (int i = 0; i < favoriteColors.Length; i++)
         favoriteColors[i] = Console.ReadLine();
 
     return favoriteColors;
+
 }
 
 static bool CheckingValueCorrectEntered(string value, out int intValue)
 {
-    if (int.TryParse(value, out int intNum))
+    if (int.TryParse(value, out int intNum) && intNum > 0)
     {
-            if (intNum > 0)
-            {
-                intValue = intNum;
-                return false;
-            }
+        intValue = intNum;
+        return false;
     }
+    else
     {
         intValue = 0;
         return true;
     }
 }
 
-static void ShowResult(survey)
+static void ShowResult((string name, string surname, int age, int quantityOfPets, string[] nicknames, int quantityOfFavoriteColors, string[] favoriteColors) survey)
 {
-    
+    // Вывод результатов опроса
+    Console.WriteLine($"Имя: {survey.name}");
+    Console.WriteLine($"Фамилия: {survey.surname}");
+    Console.WriteLine($"Возраст: {survey.age}");
+
+    if (survey.quantityOfPets > 0)
+    {
+        Console.WriteLine("Домашние питомцы:");
+        for (int i = 0; i < survey.quantityOfPets; i++)
+        {
+            Console.WriteLine($"Питомец {i + 1}: {survey.nicknames[i]}");
+        }
+    }
+
+    Console.WriteLine("Любимые цвета:");
+    for (int i = 0; i < survey.quantityOfFavoriteColors; i++)
+    {
+        Console.WriteLine($"Цвет {i + 1}: {survey.favoriteColors[i]}");
+    }
+
 }
+
+// Вызов функции опроса и вывод результатов
+var surveyResult = Survey();
+ShowResult(surveyResult);
